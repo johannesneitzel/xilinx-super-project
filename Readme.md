18dd868b3895f0f6fa0a6155bc5935a87bec2aac
Johannes <software@johannesneitzel.de>
2026-06-04 11:30:33 +0200

    fyc

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-test
+ proejct sets up Xilix yocto env based on "yocto-manifests" xml.
+
+1. source setup script: . setup.sh
--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ repo init -u https://github.com/johannesneitzel/yocto-manifests.git -b rel-v2025
 repo sync -j4
 echo 'source setupsdk'
 source setupsdk
+echo 'BB_NUMBER_THREADS = "4"' >> ./build/conf/local.conf
+echo 'PARALLEL_MAKE = "-j 4"' >> ./build/conf/local.conf
 #echo 'BBLAYERS += "${TOPDIR}/../sources/meta-custom-xilinx"' >> build/conf/bblayers.conf
 echo 'Done! Now MACHINE (default is: "qemu-zynqmp") can be set in build/conf/local.conf and run bitbake.'
