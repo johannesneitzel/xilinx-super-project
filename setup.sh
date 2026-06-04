33fc52b9f8ed80bd158c95e8df14cd101e2c3af6
Johannes <software@johannesneitzel.de>
2026-06-04 11:20:43 +0200

    easy yocto setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+repo init -u https://github.com/johannesneitzel/yocto-manifests.git -b rel-v2025.2
+repo sync -j4
+echo 'source setupsdk'
+source setupsdk
+#echo 'BBLAYERS += "${TOPDIR}/../sources/meta-custom-xilinx"' >> build/conf/bblayers.conf
+echo 'Done! Now MACHINE (default is: "qemu-zynqmp") can be set in build/conf/local.conf and run bitbake.'
